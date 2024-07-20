4a68afac24aea1aa4831c8010bd83828cf099c9e
AuliaVirza101 <serjekenterprise@gmail.com>
2024-07-20 05:52:34 -0700

    delete feature

--- lib/utils/todo_list.dart
+++ lib/utils/todo_list.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoList extends StatelessWidget {
-const TodoList({super.key,
-required this.taskName,
-required this.taskCompleted,
-required this.onChanged,
-});
+  const TodoList({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+    required this.deleteFunction,
+  });
 
-final String taskName;
-final bool taskCompleted;
-final Function(bool?)? onChanged;
+  final String taskName;
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:const EdgeInsets.only(
-        top:20,
-        left:20,
-        right:20,
-        bottom:0,
+      padding: const EdgeInsets.only(
+        top: 20,
+        left: 20,
+        right: 20,
+        bottom: 0,
       ),
-         child: Container(
+      
+       child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
+          children: [
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ],
+        ),
+        child: Container(
           padding: const EdgeInsets.all(20),
           decoration: BoxDecoration(
             color: const Color.fromARGB(255, 19, 17, 21),
@@ -53,6 +69,7 @@ final Function(bool?)? onChanged;
             ],
           ),
         ),
+      ),
     );
   }
 }
